92cb594a6544fac5c6547cc4fc2dc1f46bb23e97
igor <i.matukin@gmail.com>
2017-07-24 01:55:00 +0300

    add files

--- CPrimerPlus/Ch09/exercise9-08.c
+++ CPrimerPlus/Ch09/exercise9-08.c
@@ -1,34 +1,43 @@
 // Chapter 9 Exercise 8
 #include <stdio.h>
 
-double power(double x, long n);	// ANSI prototype
+double power(double a, int b); /* прототип ANSI */
 
 int main(void)
 {
-	double x, xpow;
-	signed int exp;
+    double x, xpow;
+    int n;
 
-	printf("Введите число и степень, в которую число будет возведено.\n");
-	printf("Для завершения программы введите 'q'.\n");
-	while (scanf("%lf %d", &x, &exp) == 2)
-	{
-		xpow = power(x,exp);	// function call
-		printf("%.3g в степени %d равно %.5g\n", x, exp, xpow);
-		printf("Введите следующую пару чисел или 'q' для завершения.\n");
-	}
-	printf("Надеемся, что вы оценили это упражнение -- до свидания!\n");
+    printf("Введите число и целочисленную степень, в которую число будет возведено.\n");
+    printf("Либо введите 'q' для завершения.\n");
+    while (scanf("%lf%d", &x, &n) == 2)
+    {
+        xpow = power(x, n); /* вызов функции */
+        printf("%.3g в степени %d равно %.5g\n", x, n, xpow);
+        printf("Введите следующую пару чисел или 'q' для завершения.\n");
+    }
+    printf("Программа завершена.\n");
 
-	return 0;
+    return 0;
 }
-
-double power(double x, long n)
+double power(double a, int b) /* определение функции */
 {
-	if (n == 0)
-		return 1;
-	if (n < 0)
-		return power(1 / x, -n);
-	if(n % 2)
-		return x * power(x, n - 1);
+    double pow = 1;
+    int i;
 
-	return power(x * x, n / 2);
+    if (b == 0)
+    {
+        if (a == 0)
+            printf("Результат возведения 0 в степень 0 не определен; используйте 1 в качестве значения\n");
+        pow = 1.0;
+    }
+    else if (a == 0)
+        pow = 0.0;
+    else if (b > 0)
+        for (i = 1; i <= b; i++)
+            pow *= a;
+    else /* b < 0 */
+        pow = 1.0 / power(a, -b);
+
+    return pow; /* возвратить значение pow */
 }
---
+++ CPrimerPlus/Ch09/exercise9-09.c
@@ -0,0 +1,40 @@
+// Chapter 9 Exercise 9
+#include <stdio.h>
+
+double power(double x, long n);
+
+int main(void)
+{
+    double x, xpow;
+    signed int exp;
+
+    printf("Введите число и степень, в которую число будет возведено.\n");
+    printf("Для завершения программы введите 'q'.\n");
+    while (scanf("%lf %d", &x, &exp) == 2)
+    {
+        if (x == 0 && exp == 0)
+        {
+            printf("Предупреждение:\n");
+            printf("результат возведения О в степень О не определён. Используется значение 1.\n");
+        }
+        xpow = power(x, exp);
+        printf("%.3g в степени %d равно %.5g\n", x, exp, xpow);
+        printf("Введите следующую пару чисел или 'q' для завершения.\n");
+    }
+    printf("Надеемся, что вы оценили это упражнение -- до свидания!\n");
+
+    return 0;
+}
+
+double power(double x, long n)
+{
+
+    if (n == 0)
+        return 1;
+    if (n < 0)
+        return power(1 / x, -n);
+    if(n % 2)
+        return x * power(x, n - 1);
+
+    return power(x * x, n / 2);
+}
