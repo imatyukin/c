95b15424498a858070fdb8d57fa7d9ad006680e9
igor <i.matukin@gmail.com>
2017-07-23 16:11:36 +0300

    update files

--- CPrimerPlus/Ch09/exercise9-08.c
+++ CPrimerPlus/Ch09/exercise9-08.c
@@ -6,18 +6,17 @@ double power(double, long);	// ANSI prototype
 int main(void)
 {
 	double x, xpow;
-	int exp;
+	signed int exp;
 
-	printf("Enter a number and the positive integer power");
-	printf(" to which\nthe number will be raised. Enter q");
-	printf(" to quit.\n");
+	printf("Введите число и степень, в которую число будет возведено.\n");
+	printf("Для завершения программы введите 'q'.\n");
 	while (scanf("%lf%d", &x, &exp) == 2)
 	{
 		xpow = power(x,exp);	// function call
-		printf("%.3g to the power %d is %.5g\n", x, exp, xpow);
-		printf("Enter next pair of numbers or q to quit.\n");
+		printf("%.3g в степени %d равно %.5g\n", x, exp, xpow);
+		printf("Введите следующую пару чисел или 'q' для завершения.\n");
 	}
-	printf("Hope you enjoyed this power trip -- bye!\n");
+	printf("Надеемся, что вы оценили это упражнение -- до свидания!\n");
 
 	return 0;
 }
