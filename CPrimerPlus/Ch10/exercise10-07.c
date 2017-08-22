91f9f4322973ac613834bb53e5601ef1b2b18504
igor <i.matukin@gmail.com>
2017-08-23 01:41:35 +0300

    add files

--- CPrimerPlus/Ch10/exercise10-07.c
+++ CPrimerPlus/Ch10/exercise10-07.c
@@ -1,6 +1,6 @@
 /* Упражнение по программированию 10.7 */
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 double randMtoN(double M, double N);
 void print_row(double (*arr)[6], int row);
