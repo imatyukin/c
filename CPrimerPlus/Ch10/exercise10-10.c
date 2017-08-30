007b4b25ed72500eabd9ece269783b678457962a
igor <i.matukin@gmail.com>
2017-08-30 23:47:18 +0300

    add files

---
+++ CPrimerPlus/Ch10/exercise10-10.c
@@ -0,0 +1,70 @@
+/* Упражнение по программированию 10.10 */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define COLS 4
+
+int randMtoN(int M, int N);
+void sumarr(int cols, int source1[cols], int source2[cols], int target[cols]);
+void showarr(int cols, int ar[cols]);
+
+int main()
+{
+    int source1[COLS];
+    int source2[COLS];
+    int target[COLS];
+
+    // initialize source arrays
+    for (int i = 0; i < 4; i++) {
+        source1[i] = randMtoN(1, 100);
+        source2[i] = randMtoN(1, 100);
+    }
+
+    // sum of elements of source1 and source2
+    sumarr(COLS, source1, source2, target);
+
+    // display contents of arrays
+    printf("Source 1:\n");
+    showarr(COLS, source1);
+    putchar('\n');
+
+    printf("Source 2:\n");
+    showarr(COLS, source2);
+    putchar('\n');
+
+    printf("Target:\n");
+    showarr(COLS, target);
+    putchar('\n');
+
+    return 0;
+}
+
+int randMtoN(int M, int N)
+{
+    // generate a random number in the range from (M, N)
+
+    return M + (rand() / (RAND_MAX / (N - M)));
+}
+
+void sumarr(int cols, int source1[cols], int source2[cols], int target[cols])
+{
+    // sum of elements of two source arrays
+
+    int tmp;
+
+    for (int i = 0; i < cols; i++) {
+        tmp = source1[i];
+        target[i] = source2[i] + tmp;
+    }
+}
+
+void showarr(int cols, int ar[cols])
+{
+    // show the contents of a array
+
+    for (int i = 0; i < cols; i++)
+        printf("%d ", ar[i]);
+
+    putchar('\n');
+}
+
