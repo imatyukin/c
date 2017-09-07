6f34c57f4ace91e79ff14e6eaae27d7389c32545
igor <i.matukin@gmail.com>
2017-09-07 22:23:24 +0300

    Did chapter 10 exercise 12.

---
+++ CPrimerPlus/Ch10/exercise10-12.c
@@ -0,0 +1,63 @@
+/* Упражнение по программированию 10.12 */
+#include <stdio.h>
+
+#define MONTHS 12    // number of months in a year
+#define YEARS   5    // number of years of data
+
+void print_annual_average(int years, int months, const float data[years][months]);
+void print_monthly_averages(int years, int months, const float data[years][months]);
+
+int main(void)
+{
+    // initializing rainfall data for 2010 - 2014
+    const float rain[YEARS][MONTHS] =
+            {
+                    {4.3,4.3,4.3,3.0,2.0,1.2,0.2,0.2,0.4,2.4,3.5,6.6},
+                    {8.5,8.2,1.2,1.6,2.4,0.0,5.2,0.9,0.3,0.9,1.4,7.3},
+                    {9.1,8.5,6.7,4.3,2.1,0.8,0.2,0.2,1.1,2.3,6.1,8.4},
+                    {7.2,9.9,8.4,3.3,1.2,0.8,0.4,0.0,0.6,1.7,4.3,6.2},
+                    {7.6,5.6,3.8,2.8,3.8,0.2,0.0,0.0,0.0,1.3,2.6,5.2}
+            };
+
+    print_annual_average(YEARS, MONTHS, rain);
+    print_monthly_averages(YEARS, MONTHS, rain);
+
+    return 0;
+}
+
+void print_annual_average(int years, int months, const float data[years][months])
+{
+    int year, month;
+    float subtotal, total;
+
+    printf(" YEAR    RAINFALL  (inches)\n");
+    for (year = 0, total = 0; year < years; year++)
+    {             // for each year, sum rainfall for each month
+        for (month = 0, subtotal = 0; month < months; month++)
+            subtotal += data[year][month];
+        printf("%5d %15.1f\n", 2010 + year, subtotal);
+        total += subtotal; // total for all years
+    }
+
+    printf("\nThe yearly average is %.1f inches.\n\n",
+           total/years);
+}
+
+void print_monthly_averages(int years, int months, const float data[years][months])
+{
+    int year, month;
+    float subtotal;
+
+    printf("MONTHLY AVERAGES:\n\n");
+
+    printf(" Jan  Feb  Mar  Apr  May  Jun  Jul  Aug  Sep  Oct ");
+    printf(" Nov  Dec\n");
+    for (month = 0; month < months; month++)
+    {             // for each month, sum rainfall over years
+        for (year = 0, subtotal =0; year < years; year++)
+            subtotal += data[year][month];
+        printf("%4.1f ", subtotal/years);
+    }
+
+    printf("\n");
+}
