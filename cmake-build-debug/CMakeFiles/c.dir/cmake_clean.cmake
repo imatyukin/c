0af9d5f87cf417ff6f198e277c8fa26cbca11b9e
igor <i.matukin@gmail.com>
2017-09-10 13:50:45 +0300

    Remove files

---
+++ cmake-build-debug/CMakeFiles/c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c.dir/CPrimerPlus/Ch10/exercise10-14.c.o"
+  "c.pdb"
+  "c"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
