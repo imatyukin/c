1fc07c7fd85c81d419baee844048e68b27b2f31d
igor <i.matukin@gmail.com>
2017-12-29 22:07:26 +0300

    Did chapter 13 exercise 9.

---
+++ CPrimerPlus/Ch13/exercise13-09.c
@@ -0,0 +1,37 @@
+// Chapter 13 Exercise 9
+/* Для упрощения подсчета в каждой строке хранится одно число и слово. */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 47
+
+int main(void)
+{
+    FILE *fp;
+    char words[MAX];
+    int wordct = 0;
+
+    if ((fp = fopen("wordy", "a+")) == NULL)
+    {
+        fprintf(stderr,"Не удается открыть файл \"wordy\".\n");
+        exit(EXIT_FAILURE);
+    }
+    // определить текущее количество строк
+    rewind(fp);
+    while (fgets(words, MAX, fp) != NULL)
+        wordct++;
+    rewind(fp);
+    puts("Введите слова для добавления в файл; для завершения");
+    puts("введите символ # в начале строки.");
+    while ((fscanf(stdin,"%40s", words) == 1) && (words[0] != '#'))
+        fprintf(fp, "%3d: %s\n", ++wordct, words);
+    puts("Содержимое файла:");
+    rewind(fp); // возврат в начало файла
+    while (fgets(words, MAX, fp) != NULL) // чтение строки, включая число
+        fputs(words, stdout);
+    if (fclose(fp) != 0)
+        fprintf(stderr,"Ошибка при закрытии файла\n");
+    puts("Программа завершена.");
+
+    return 0;
+}
