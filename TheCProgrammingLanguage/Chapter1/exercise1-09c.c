05bc549d80f18cdc85977947dd9c4a0ec0f31189
igor <i.matukin@gmail.com>
2018-03-25 00:08:28 +0300

    Exercise 1.9

---
+++ TheCProgrammingLanguage/Chapter1/exercise1-09c.c
@@ -0,0 +1,18 @@
+// Exercise 1-9. Write a program to copy its input to its output, replacing each string of one or more blanks by a
+// single blank.
+#include <stdio.h>
+
+#define NONBLANK 'a'
+
+/* replace string of blanks with a single blank         */
+int main()
+{
+    int c, lastc;
+
+    lastc = NONBLANK;
+    while ((c = getchar()) != EOF) {
+        if (c != ' ' || lastc != ' ')
+            putchar(c);
+        lastc = c;
+    }
+}
