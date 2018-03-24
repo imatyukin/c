9a9a758c76d4603493606e5b33d8c1625f9eacc0
igor <i.matukin@gmail.com>
2018-03-24 23:20:54 +0300

    Exercise 1.4

--- TheCProgrammingLanguage/Chapter1/exercise1-04.c
+++ TheCProgrammingLanguage/Chapter1/exercise1-04.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 
 /* print Celsius-Fahrenheit table
-    for celsius = 0, 20, ..., 300; floating-point version */
+    for celsius = 0, 20, ..., 300; floating-point version   */
 int main(void)
 {
     float fahr, celsius;
     float lower, upper, step;
 
-    lower = 0;      /* lower limit of temperature scale */
-    upper = 300;    /* upper limit */
-    step = 20;      /* step size */
+    lower = 0;          /* lower limit of temperature scale */
+    upper = 300;        /* upper limit                      */
+    step = 20;          /* step size                        */
 
     printf("Celsius Fahrenheit\n");
     celsius = lower;
