e0a8922cb6f8bed8b2fab9347083a3fe05af7d11
igor <i.matukin@gmail.com>
2018-05-24 23:47:48 +0300

    Exercise 2.5

--- TheCProgrammingLanguage/Chapter2/exercise2-05.c
+++ TheCProgrammingLanguage/Chapter2/exercise2-05.c
@@ -9,7 +9,7 @@ int any(char s1[], char s2[])
 
     for (i = 0; s1[i] != '\0'; i++)
         for (j = 0; s2[j] != '\0'; j++)
-            if (s1[i] == s2[j])         /*  match foind?            */
+            if (s1[i] == s2[j])         /*  match found?            */
                 return i;               /*  location first match    */
     return -1;                          /*  otherwise, no match     */
 }
