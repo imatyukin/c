048041475ac06709b24459c8435cd6e6d5cdfcac
igor <i.matukin@gmail.com>
2018-05-24 23:46:55 +0300

    Exercise 2.5

---
+++ TheCProgrammingLanguage/Chapter2/exercise2-05.c
@@ -0,0 +1,15 @@
+// Exercise 2-5. Write the function any(s1,s2), which returns the first location in a string s1
+// where any character from the string s2 occurs, or -1 if s1 contains no characters from s2. (The
+// standard library function strpbrk does the same job but returns a pointer to the location.)
+
+/*  any: return first location in s1 where any char from s2 occurs  */
+int any(char s1[], char s2[])
+{
+    int i, j;
+
+    for (i = 0; s1[i] != '\0'; i++)
+        for (j = 0; s2[j] != '\0'; j++)
+            if (s1[i] == s2[j])         /*  match foind?            */
+                return i;               /*  location first match    */
+    return -1;                          /*  otherwise, no match     */
+}
