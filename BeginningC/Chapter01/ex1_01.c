55300557e85f2b52683387ce3ac107d967a73069
igor <i.matukin@gmail.com>
2019-02-19 21:34:21 +0300

    Exercises

---
+++ BeginningC/Chapter01/ex1_01.c
@@ -0,0 +1,10 @@
+// Exercise 1-1.
+// Write a program that will output your name and address using a separate printf() statement for each line of output.
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Igor\n");
+    printf("Russia, Saint Petersburg");
+    return 0;
+}
