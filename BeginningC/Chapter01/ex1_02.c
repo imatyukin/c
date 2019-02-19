14f4c60a3dac92ac9ae468efb93fdf201e29f7b7
igor <i.matukin@gmail.com>
2019-02-19 21:38:18 +0300

    Exercises

---
+++ BeginningC/Chapter01/ex1_02.c
@@ -0,0 +1,9 @@
+// Exercise 1-2.
+// Modify your solution for the previous exercise so that it produces all the output using only one printf() statement.
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Igor\nRussia, Saint Petersburg");
+    return 0;
+}
