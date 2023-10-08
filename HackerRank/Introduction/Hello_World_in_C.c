d4df9f9466634af2f895c820e704598c81311d72
imatyukin <igormatyukin@yandex.ru>
2023-10-09 00:18:05 +0300

    "Hello World!" in C

--- HackerRank/Introduction/Hello_World_in_C.c
+++ HackerRank/Introduction/Hello_World_in_C.c
@@ -2,8 +2,9 @@
 
 /* Task
 
-This challenge requires you to print Hello, World! on a single line, and then print the already provided 
-input string to stdout. If you are not familiar with C, you may want to read about the printf() command. */
+This challenge requires you to print Hello, World! on a single line, and then 
+print the already provided input string to stdout. If you are not familiar with C, 
+you may want to read about the printf() command. */
 
 /* Input (stdin)
 
